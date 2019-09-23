f7061e013656c66c2330a389e1bda8e62d966776
SujithN23 <sujithnadaraj238@gmail.com>
2019-09-23 15:44:42 +0530

    second commit

---
+++ SalesTool/src/com/faith/test/Main.java
@@ -0,0 +1,15 @@
+package com.faith.test;
+
+public class Main {
+	public static void main() {
+	SalesData objSalesData=new SalesData();
+	System.out.println("Hello...");
+	objSalesData.display();
+	displayGreeting();
+	
+	
+	}
+private static void displayGreeting() {
+	System.out.println("Hello ust guys");
+}
+}
